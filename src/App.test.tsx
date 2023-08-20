795502c96350d165d2c56ff8950954b59d682a5a
Takuma Kira <takuma@kirakiraworx.com>
2023-08-20 16:33:04 +0900

    [dev] Implement the main functionality and tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,41 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { act, render, screen } from '@testing-library/react';
+import App, { ADDING_BUTTON_DURATION, START_BUTTON_LABEL, STOP_BUTTON_LABEL } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+/**
+ * Move of DOMRect cannot be detected in React Testing Library as all the elements don't have non-zero size.
+ * That's why we don't test if buttons are disabled right after their move.
+ */
+
+afterEach(() => {
+  jest.clearAllTimers()
+  jest.useRealTimers()
+})
+
+describe('<App />', () => {
+  test('renders start and stop button', () => {
+    render(<App />)
+    const startButton = screen.getByText(START_BUTTON_LABEL)
+    expect(startButton).toBeInTheDocument()
+    const clearButton = screen.getByText(STOP_BUTTON_LABEL)
+    expect(clearButton).toBeInTheDocument()
+  })
+
+  test('adds buttons if start button is clicked', async () => {
+    jest.useFakeTimers()
+    render(<App />)
+    act(() => screen.getByText(START_BUTTON_LABEL).click())
+    act(() => jest.advanceTimersByTime(ADDING_BUTTON_DURATION))
+    expect(await screen.findByText('0')).toBeInTheDocument()
+  })
+
+  test('clears buttons when stop button is clicked', async () => {
+    jest.useFakeTimers()
+    render(<App />)
+    act(() => screen.getByText(START_BUTTON_LABEL).click())
+    act(() => jest.advanceTimersByTime(ADDING_BUTTON_DURATION))
+    expect(await screen.findByText('0')).toBeInTheDocument()
+    act(() => screen.getByText(STOP_BUTTON_LABEL).click())
+    act(() => jest.advanceTimersByTime(ADDING_BUTTON_DURATION))
+    expect(screen.queryByText('0')).not.toBeInTheDocument()
+  })
+})
